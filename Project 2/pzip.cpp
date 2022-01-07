7ac85c7118c14645148b31e44c9c8a346331b347
Ziad El3ssal <84142720+ZiadSENG@users.noreply.github.com>
2022-01-07 23:01:30 +0200

    Update pzip.cpp

--- Project 2/pzip.cpp
+++ Project 2/pzip.cpp
@@ -1,8 +1,3 @@
-/*
-1-if file cannot be opened what will we do ?
-2-after every file is compressed should us compin  them or save in seperated files ?
-
-*/
 #include <iostream>
 #include <fstream>
 #include <bits/stdc++.h>
@@ -19,21 +14,30 @@
 #include <mutex>
 
 using namespace std;
+
 struct ftype
 {
     FILE *filename;
     int current; // to detemrmine where i have ended last time still under progress
 };
+
 struct dat
 {
     int num;
     char c;
 };
+
+
 pthread_t threads[100];
 vector<dat> v[100];
 
 void *zip(void * arg)
 {
+    // to zip file
+    // detemine where it is possible to have race condition
+    // make locks for this race condition
+    // be aware of that the declration of any variable here is vanished after it return
+    // instead of lock we may use pthread_cond this will make it easier to code
     ftype f= *(ftype*)arg;
     int idx=f.current;
     FILE* fp=f.filename;
@@ -55,8 +59,6 @@ void *zip(void * arg)
             c=fgetc(fp);
             continue;
         }
-        //cout<<cur;
-
         cnt++;
         c=fgetc(fp);
     }
@@ -66,15 +68,7 @@ void *zip(void * arg)
     v[idx].push_back(psh);
     curr=c;
     cnt=1;
-    //cout<<"@"<<cur<<" "<<s<<endl;
-
-    //double f=(double)e/CLOCKS_PER_SEC;
     return 0;
-    // to zip file
-    // detemine where it is possible to have race condition
-    // make locks for this race condition
-    // be aware of that the declration of any variable here is vanished after it return
-    // instead of lock we may use pthread_cond this will make it easier to code
 }
 
 int main(int argc, char **argv)
@@ -99,21 +93,11 @@ int main(int argc, char **argv)
         pthread_create(&threads[i],NULL,zip,&files[i]);
     }
     // make threads for every input file
-    /*for(int i=1; i<argc; i++)
-    {
-
-        //pthread_join(threads[i],NULL);// create thread for each input file
-    }*/
-    //cout<<"W";
     if(argc>2)for(int i=1; i<argc; i++)
     {
         pthread_join(threads[i],NULL);
         fclose(files[i].filename);//make the main wait untill the thread is done the compression us done
     }
-    /* cout<<v[1][0].num<<endl;
-
-      cout<<v[2][0].num<<endl;
-       cout<<v[3][0].num<<endl;*/
     for(int i=2; i<argc; i++)
     {
         int sz1=v[i-1].size()-1;
@@ -123,7 +107,6 @@ int main(int argc, char **argv)
             v[i-1].pop_back();
         }
     }
-    //FILE* fpp=freopen("out.out","w",stdout);
     for(int i=1; i<argc; i++)
     {
 
@@ -135,7 +118,5 @@ int main(int argc, char **argv)
 
         }
     }
-
-//    fclose(fpp);
     return 0;
 }
